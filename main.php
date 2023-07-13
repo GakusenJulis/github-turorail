4c4c40bc4cca5f57cef686d2b3f7b041d2413dc7
GakusenJulis <kusushihunkihun.jp@gmail.com>
2023-07-13 21:54:22 +0700

    1

--- main.php
+++ main.php
@@ -1,3 +1,5 @@
-<?php echo"My Index" ?>
-<?php echo"Julis" ?>
-<?php echo"hahah" ?>
+<?php echo"Yodsapat Jirachaiphankul 1" ?>
+<?php echo"Yodsapat Jirachaiphankul 2" ?>
+<?php echo"Yodsapat Jirachaiphankul 3" ?>
+<?php echo"Yodsapat Jirachaiphankul 4" ?>
+<?php echo"Yodsapat Jirachaiphankul 5" ?>
